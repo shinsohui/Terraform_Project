416b91a8c3d557269f6463b2fac7e04f2c8909ff
seonwoojh <swjh9523@gmail.com>
2022-04-26 19:30:08 +0900

    provider.tf

--- azure/provider.tf
+++ azure/provider.tf
@@ -13,9 +13,9 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "0e337d72-f370-4cb8-9749-9968126b71df"
-  client_id       = "4b3914fd-9548-430d-afce-12cbf51c382b"
-  client_secret   = "87Y-NV3QsuI1s03hrCqI-6OIgxPY_cUu0r"
-  tenant_id       = "40c79e45-8666-45ec-9af1-b4a931926789"
+  subscription_id = 
+  client_id       = 
+  client_secret   = 
+  tenant_id       = 
 
 }
